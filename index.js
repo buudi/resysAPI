716aa62c4f75d6b2c5fc6d59bd555703c36eda46
Abdullah Yaser <38732824+buudi@users.noreply.github.com>
2023-05-28 13:24:09 +0800

    created ID generator

--- index.js
+++ index.js
@@ -1,24 +1,29 @@
-const express = require('express');
-const cors = require('cors');
-const mountRoutes = require('./routes');
+const express = require("express");
+const mountRoutes = require("./routes");
 
 const app = express();
 const port = 3000;
 
-const corsOptions = {
-    origin: ['http://localhost:5173','http://localhost:5174'],
-    methods: ['GET', 'POST', 'PUT', 'DELETE'],
-};
-app.use(cors(corsOptions));
+// todo: modify the origins when connecting to the front end server
+// const cors = require("cors");
+// const corsOptions = {
+//   origin: ["http://localhost:5173", "http://localhost:5174"],
+//   methods: ["GET", "POST", "PUT", "DELETE"],
+// };
+// app.use(cors(corsOptions));
 
 app.use(express.json());
 
 mountRoutes(app);
 
-app.get('/', (req, res)=>{
-    res.send(`system beta - server`)
+app.get("/", (req, res) => {
+  res.send(`system beta - server`);
 });
 
-app.listen(port, ()=>{
-    console.log(`server-beta listening on port ${port}`)
-})
+app.listen(port, () => {
+  console.log(`server-beta listening on port ${port}`);
+});
+
+
+const IDgenerator = require("./IDgenerator");
+console.log(IDgenerator.generateApartmentId());
