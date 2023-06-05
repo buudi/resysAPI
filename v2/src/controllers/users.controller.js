33d4106e65eb6485386e25439a01a4fb0728cafb
Abdullah Yaser <38732824+buudi@users.noreply.github.com>
2023-06-05 15:45:23 +0800

    refactored getAllUsers()

--- v2/src/controllers/users.controller.js
+++ v2/src/controllers/users.controller.js
@@ -6,53 +6,31 @@ class UsersController {
         this.model = require(`${this.basePath}/main/users.model`);
     }
 
-
     getAllUsers = async (req, res, next) => {
-        const {username} = req.query;
-        const {role} = req.query;
-        if(username){
-            try {
-                // todo: query the db for the user with the username
-                const user = await this.model.findUserByUsername(username);
-                if (user instanceof Error)
-                    throw new Error(`Error getting user: ${user.message}`);
+        const { username, role } = req.query;
 
-                res.status(200).json(user.rows);
-            } catch (error) {
-                next(error);
+        try {
+            let users;
+
+            if (username) {
+                users = await this.model.findUserByUsername(username);
+            } else if (role) {
+                users = await this.model.findUserByRole(role);
+            } else {
+                users = await this.model.getAllUsers();
             }
-        } else if(role){
-            try {
-                const users = await this.model.findUserByRole(role);
-                if (users instanceof Error)
-                    throw new Error(`Error getting users: ${users.message}`);
 
-                res.status(200).json(users.rows);
-            } catch (error) {
-                next(error);
+            if (users instanceof Error) {
+                throw new Error(`Error getting users: ${users.message}`);
             }
-        } else {
-            try {
 
-                // 1. query the database
-                const users = await this.model.getAllUsers();
-
-                // 2. if the database returned an error, throw an error
-                if (users instanceof Error)
-                    throw new Error(`Error getting users: ${users.message}`);
-
-                // 3. else return the users
-                res.status(200).json(
-                    users.rows
-                );
-
-            } catch (error) {
-                next(error);
-            }
+            res.status(200).json(users.rows);
+        } catch (error) {
+            next(error);
         }
-    }
+    };
 
-
+    // todo: encrypt passwords with bcrypt
     createUser = async (req, res, next) => {
         try {
             // 1. get the data from the request body
@@ -78,6 +56,9 @@ class UsersController {
     }
 
 
+    // add more methods here
+    // ....
+    // ....
 
 }
 
